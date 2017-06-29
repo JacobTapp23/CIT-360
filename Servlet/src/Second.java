0799c464555e80b7314c73dd01e0c61ebe655274
BarzeeRex <barzeer@byui.edu>
2017-06-29 16:36:52 -0600

    Initial upload of a simple HTTP Servlet named Second. This simple servlet puts the request and header information into an HTML document and sends the document back to the client.

---
+++ Servlet/src/Second.java
@@ -0,0 +1,109 @@
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+public class Second extends HttpServlet {
+	private static final String
+			doctype = "<!DOCTYPE HTML>\n",
+			html = "<html lang=\"en-us\">\n",
+			_html = "</html>\n",
+			head = "<head>\n",
+			_head = "</head>\n",
+			charset = "<meta charset=\"utf-8\">\n",
+			author = "<meta name=\"author\" content=\"Second Servlet\">\n",
+			title = "<title>",
+			_title = "</title>\n",
+			body = "<body>\n",
+			_body = "</body>\n",
+			h1 = "<h1>",
+			_h1 = "</h1>\n",
+			h2 = "<h2>",
+			_h2 = "</h2>\n",
+			p = "<p>",
+			_p = "</p>\n",
+			div = "<div>",
+			_div = "</div>\n";
+	private int requestNum = 0;
+
+	@Override
+	public void init() {
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		PrintWriter out = resp.getWriter();
+		out.print(doctype);
+		out.print(html);
+		out.print(head);
+		out.print(charset);
+		out.print(author);
+		out.print(title + "Second Servlet" + _title);
+		out.print(_head);
+
+		out.print(body);
+		out.print(h1 + "Second Servlet" + _h1);
+
+		out.print(h2 + "User Principal" + _h2);
+		out.print(div + req.getUserPrincipal() + _div);
+
+		out.print(h2 + "Remote User" + _h2);
+		out.print(div + req.getRemoteUser() + _div);
+
+		out.print(h2 + "URL" + _h2);
+		out.print(div + req.getRequestURL() + _div);
+
+		out.print(h2 + "Method" + _h2);
+		out.print(div + req.getMethod() + _div);
+
+		out.print(h2 + "Session ID" + _h2);
+		out.print(div + req.getRequestedSessionId() + _div);
+		out.print(div + "from cookie: "
+						+ req.isRequestedSessionIdFromCookie() + _div);
+		out.print(div + "from URL: "
+				+ req.isRequestedSessionIdFromURL() + _div);
+		out.print(div + "is valid: "
+				+ req.isRequestedSessionIdValid() + _div);
+
+		HttpSession session = req.getSession();
+		if (session != null) {
+			out.print(h2 + "Session" + _h2);
+			out.print(div + "id: " + session.getId() + _div);
+			out.print(div + "creation: " + session.getCreationTime() + _div);
+			out.print(div + "accessed: " + session.getLastAccessedTime() +
+					_div);
+			out.print(div + "max interval: "
+					+ session.getMaxInactiveInterval() + _div);
+		}
+
+		out.print(h2 + "Header" + _h2);
+		Enumeration<String> names = req.getHeaderNames();
+		while (names.hasMoreElements()) {
+			String name = names.nextElement();
+			String value = req.getHeader(name);
+			out.print(div + name + ": " + value + _div);
+		}
+
+		Cookie[] cookies = req.getCookies();
+		if (cookies != null) {
+			out.print(h2 + "Cookies" + _h2);
+			for (Cookie c : req.getCookies()) {
+				out.println(div + c.getName() + " " + c.getValue() + _div);
+			}
+		}
+
+		out.print(h2 + "Request Number" + _h2);
+		out.print(p + nextRequestNumber() + _p);
+
+		out.print(_body);
+		out.print(_html);
+	}
+
+	private synchronized int nextRequestNumber() {
+		return ++requestNum;
+	}
+
+	@Override
+	public void destroy() {
+	}
+}
---
+++ Servlet/web/index.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+<p>Are ordinary tags allowed in a JSP?</p>
+  $END$
+  </body>
+</html>
