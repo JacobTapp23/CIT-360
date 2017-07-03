1186cccb1c121cf330d13540342fa841d0955e81
BarzeeRex <barzeer@byui.edu>
2017-07-03 17:13:29 -0600

    Add additional explanation of JUnit.

--- UnitTesting/src/TestAccount.java
+++ UnitTesting/src/TestAccount.java
@@ -1,10 +1,19 @@
 import java.util.Date;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-/** A class to test the Account class
- */
+/** TestAccount is a class that uses JUnit to test the Account class. */
 public class TestAccount {
+	// A JUnit test is simply a function that is annotated with @Test
+	// and that takes no parameters. This class contains four JUnit
+	// tests.  Within a test function, a developer uses the assert*
+	// functions to verify that his code is working correctly. Notice
+	// that in the testDeposit and testWithdraw functions, the
+	// assertEquals function is used to verify that the balance of an
+	// account is correct after .deposit and .withdraw are executed by
+	// the computer.
+
 
 	/** Verifies that account identification numbers are assigned
 	 * starting with 1, sequentially in the order that the accounts
@@ -42,10 +51,14 @@ public class TestAccount {
 		assertEquals(bal, a.getBalance(), delta);
 
 		// Attempt to deposit a negative amount.
+		boolean thrown = false;
 		try {
 			a.deposit(-10);
 		}
-		catch (Exception ex) { }
+		catch (Exception ex) {
+			thrown = true;
+		}
+		assertTrue(thrown);
 		assertEquals(bal, a.getBalance(), delta);
 
 		// Deposit $50.01
@@ -56,9 +69,13 @@ public class TestAccount {
 
 		// Attempt to deposit a negative amount, again.
 		try {
+			thrown = false;
 			a.deposit(-10);
 		}
-		catch (Exception ex) { }
+		catch (Exception ex) {
+			thrown = true;
+		}
+		assertTrue(thrown);
 		assertEquals(bal, a.getBalance(), delta);
 
 		// Deposit $30.31
@@ -77,10 +94,14 @@ public class TestAccount {
 		double bal = 0;
 
 		// Attempt to withdraw a negative amount.
+		boolean thrown = false;
 		try {
 			a.withdraw(-10);
 		}
-		catch (Exception ex) { }
+		catch (Exception ex) {
+			thrown = true;
+		}
+		assertTrue(thrown);
 		assertEquals(bal, a.getBalance(), delta);
 
 		double amt = 40.25;
@@ -90,9 +111,13 @@ public class TestAccount {
 
 		// Attempt to withdraw a negative amount, again.
 		try {
+			thrown = false;
 			a.withdraw(-10);
 		}
-		catch (Exception ex) { }
+		catch (Exception ex) {
+			thrown = true;
+		}
+		assertTrue(thrown);
 		assertEquals(bal, a.getBalance(), delta);
 
 		// Withdraw $10.15
@@ -102,11 +127,15 @@ public class TestAccount {
 		assertEquals(bal, a.getBalance(), delta);
 
 		// Attempt to withdraw $40.00
-		amt = 40;
 		try {
+			amt = 40;
+			thrown = false;
 			a.withdraw(amt);
 		}
-		catch (Exception ex) { }
+		catch (Exception ex) {
+			thrown = true;
+		}
+		assertTrue(thrown);
 		assertEquals(bal, a.getBalance(), delta);
 
 		// Withdraw the remaining balance.
@@ -117,11 +146,15 @@ public class TestAccount {
 
 		// Attempt to withdraw another $30.00,
 		// even though the balance is zero.
-		amt = 30;
 		try {
+			amt = 30;
+			thrown = false;
 			a.withdraw(amt);
 		}
-		catch (Exception ex) { }
+		catch (Exception ex) {
+			thrown = true;
+		}
+		assertTrue(thrown);
 		assertEquals(bal, a.getBalance(), delta);
 	}
 }
