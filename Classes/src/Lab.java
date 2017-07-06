aae36740e746cf956fe7ebc6c32eb65a7e58ad12
BarzeeRex <barzeer@byui.edu>
2017-07-06 16:04:27 -0600

    Separate the instructions from the Java code to make it easier for students to view the instructions and code side by side.

--- Classes/src/Lab.java
+++ Classes/src/Lab.java
@@ -1,78 +1,8 @@
-/* Your boss has asked you to write molecular simulation software, no
-small task.
-1. Open the IntelliJ IDE which you should have previously installed.
-2. Create a new Java Project named Lab inside your bitbucket repository.
-3. Write a Java class named "Particle".
-   a. This class must have the following private properties:
-      i. mass - type double
-      ii. charge - type double
-   b. The Particle class must have a constructor that accepts and sets values
-      for each of the three properties.
-   c. The Particle class must have getter and setter methods for each of the
-      properties.
-   d. The Particle class must have a toString method that overrides the
-      toString method in class Object.
-4. Write a Java class named "Proton".
-   a. This class must inherit from the Particle class.
-   b. The Proton class must have a constructor that takes no parameters
-      that calls the Particle constructor and passes 1.673 and 1.602 as the
-      mass and charge.
-   c. The Proton class must have a "toString" method that takes no
-      parameters and returns a String description of the Proton. The
-      description must include the name of the class, and inherited
-      properties, and the value for each of those properties. For example,
-      "proton mass= ... charge="
-   d. The Proton class must have a toString method that overrides the
-      toString method in class Particle.
-5. Write a Java class named "Neutron".
-   a. This class must inherit from the Particle class.
-   b. The Neutron class must have a constructor that takes no parameters
-      and calls the Particle constructor and passes 1.675 and 0 as the
-      mass and charge.
-   c. The Neutron class must have a "toString" method that takes no
-      parameters and returns a String description of the Neutron. The
-      description must include the name of the class, and inherited
-      properties and the value for each of those properties. For example,
-      "neutron mass= ... charge="
-   d. The Neutron class must have a toString method that overrides the
-      toString method in class Particle.
-6. Write a Java class named "Electron".
-   a. This class must inherit from the Particle class.
-   b. The Electron class must have a constructor that takes no parameters
-      that calls the Particle constructor and passes 9.109 and -1.602 as the
-      mass and charge.
-   c. The Electron class must have a "toString" method that takes no
-      parameters and returns a String description of the Electron. The
-      description must include the name of the class, and inherited
-      properties, and the value for each of those properties. For example,
-      "electron mass= ... charge="
-   d. The Electron class must have a toString method that overrides the
-      toString method in class Particle.
-7. Write a class named "Atom".
-   a. This class must have four private attributes:
-      i. name - type String
-      ii. protons - an array of Proton objects
-      iii. neutrons - an array of Neutron objects
-      iv. electrons - an array of Electron objects
-   b. class Atom must have a constructor that initializes the four attributes.
-   c. class Atom must have a toString method that overrides the toString
-      method in class Object.
-8. Write a class named "Molecule".
-   a. This class must have two private attributes:
-      i. name - type String
-      ii. atoms - an array of Atom objects
-   b. class Molecule must have a constructor that initializes the two
-      attributes.
-   c. class Atom must have a toString method that overrides the toString
-      method in class Object.
-9. Write a class named "Lab".
-   a. class Lab must contain the main method. The main method must do
-   the following:
-      i. Create a water Molecule that has two hydrogen Atoms and one
-         Oxygen atom.
-      ii. Use the toString methods to print the contents of the water
-         Molecule to the console.
+/* This file contains classes that demonstrate inheritance, method
+ * overriding, and creating objects. For the complete set of student
+ * instructions, see the file instructions.txt
  */
+
 public class Lab {
 	public static void main(String[] args) {
 		Molecule water = new Molecule("water", 3);
@@ -177,16 +107,19 @@ class Atom {
 	public Atom(String name, int protons, int neutrons, int electrons) {
 		this.name = name;
 
+		// Create the array of protons for this atom.
 		this.protons = new Proton[protons];
 		for (int i = 0;  i < this.protons.length;  ++i) {
 			this.protons[i] = new Proton();
 		}
 
+		// Create the array of neutrons for this atom.
 		this.neutrons = new Neutron[neutrons];
 		for (int i = 0;  i < this.neutrons.length;  ++i) {
 			this.neutrons[i] = new Neutron();
 		}
 
+		// Create the array of electrons for this atom.
 		this.electrons = new Electron[electrons];
 		for (int i = 0;  i < this.electrons.length;  ++i) {
 			this.electrons[i] = new Electron();
@@ -212,6 +145,7 @@ class Molecule {
 		this.atoms = new Atom[numberOfAtoms];
 	}
 
+	/** Adds one Atom to this Molecule. */
 	public void addAtom(int index, Atom toAdd) {
 		this.atoms[index] = toAdd;
 	}
