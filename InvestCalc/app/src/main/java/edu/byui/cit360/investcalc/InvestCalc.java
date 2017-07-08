f1da48a05ce96b064c6604929bd91a87bf92f3a1
BarzeeRex <barzeer@byui.edu>
2017-07-07 18:07:47 -0600

    Add a loan calculator activity to this simple client side only app.

--- InvestCalc/app/src/main/java/edu/byui/cit360/investcalc/InvestCalc.java
+++ InvestCalc/app/src/main/java/edu/byui/cit360/investcalc/InvestCalc.java
@@ -1,36 +1,33 @@
 package edu.byui.cit360.investcalc;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class InvestCalc extends Activity {
     private EditText numPrinc, numAR, numYears, numPPY;
-    private Button btnFV, btnReset;
     private TextView txtFV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
+        setContentView(R.layout.invest_calc);
 
         numPrinc = (EditText)findViewById(R.id.numPrinc);
         numAR = (EditText)findViewById(R.id.numAR);
         numYears = (EditText)findViewById(R.id.numYears);
         numPPY = (EditText)findViewById(R.id.numPPY);
-        btnFV = (Button)findViewById(R.id.btnFV);
         txtFV = (TextView)findViewById(R.id.txtFV);
-        btnReset = (Button)findViewById(R.id.btnReset);
     }
 
     public void onFVClick(View view) {
-        double a = Double.parseDouble(numPrinc.getText().toString());
-        double ar = Double.parseDouble(numAR.getText().toString());
-        int y = Integer.parseInt(numYears.getText().toString());
-        int ppy = Integer.parseInt(numPPY.getText().toString());
+        double a = LoanCalc.getDec(numPrinc);
+        double ar = LoanCalc.getDec(numAR) / 100.0;
+        int y = LoanCalc.getInt(numYears);
+        int ppy = LoanCalc.getInt(numPPY);
         double r = ar / ppy;
         int n = y * ppy;
         double fv = a * Math.pow(1 + r, n);
@@ -44,5 +41,10 @@ public class MainActivity extends Activity {
         numYears.setText("");
         numPPY.setText("");
         txtFV.setText("");
+    }
+
+    public void onLoanClick(View view) {
+        Intent intent = new Intent(this, LoanCalc.class);
+        startActivity(intent);
     }
 }
