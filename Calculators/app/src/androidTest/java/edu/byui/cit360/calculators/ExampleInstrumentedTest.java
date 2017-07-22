ac1479a40895e4c464692de41988d223e2b4ab3b
BarzeeRex <barzeer@byui.edu>
2017-07-22 13:17:03 -0600

    Hopefully finish conversion of .java and .xml files from using spaces to using tabs.

--- Calculators/app/src/androidTest/java/edu/byui/cit360/calculators/ExampleInstrumentedTest.java
+++ Calculators/app/src/androidTest/java/edu/byui/cit360/calculators/ExampleInstrumentedTest.java
@@ -19,26 +19,26 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appCtx = InstrumentationRegistry.getTargetContext();
+	@Test
+	public void useAppContext() throws Exception {
+		// Context of the app under test.
+		Context appCtx = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("edu.byui.cit360.calculators", appCtx.getPackageName());
-    }
+		assertEquals("edu.byui.cit360.calculators", appCtx.getPackageName());
+	}
 
-    @Test
-    public void testTime() {
-        double epsilon = 0.000001;
-        Quantity prop = World.getInstance().get("time");
-        assertEquals(120, prop.convert("second", 2, "minute"), epsilon);
-        assertEquals(3600, prop.convert("second", 1, "hour"), epsilon);
-        assertEquals(24 * 60, prop.convert("minute", 1, "day"), epsilon);
-        assertEquals(21, prop.convert("day", 3, "week"), epsilon);
+	@Test
+	public void testTime() {
+		double epsilon = 0.000001;
+		Quantity prop = World.getInstance().get("time");
+		assertEquals(120, prop.convert("second", 2, "minute"), epsilon);
+		assertEquals(3600, prop.convert("second", 1, "hour"), epsilon);
+		assertEquals(24 * 60, prop.convert("minute", 1, "day"), epsilon);
+		assertEquals(21, prop.convert("day", 3, "week"), epsilon);
 
-        assertEquals(2, prop.convert("minute", 120, "second"), epsilon);
-        assertEquals(1, prop.convert("hour", 3600, "second"), epsilon);
-        assertEquals(2, prop.convert("day", 2 * 24 * 60, "minute"), epsilon);
-        assertEquals(2, prop.convert("week", 14, "day"), epsilon);
-    }
+		assertEquals(2, prop.convert("minute", 120, "second"), epsilon);
+		assertEquals(1, prop.convert("hour", 3600, "second"), epsilon);
+		assertEquals(2, prop.convert("day", 2 * 24 * 60, "minute"), epsilon);
+		assertEquals(2, prop.convert("week", 14, "day"), epsilon);
+	}
 }
