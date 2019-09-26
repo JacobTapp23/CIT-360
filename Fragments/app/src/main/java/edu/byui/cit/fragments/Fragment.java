1f64efe7c1ccd809e0e09a5c9df1a46f39c6bfbf
rab29 <barzeer@byui.edu>
2019-09-26 16:34:14 -0600

    Migrate the all example apps to androidx.

--- Fragments/app/src/main/java/edu/byui/cit/fragments/Fragment.java
+++ Fragments/app/src/main/java/edu/byui/cit/fragments/Fragment.java
@@ -7,7 +7,7 @@ import org.jetbrains.annotations.NotNull;
  * This class simply adds functionality to the Android Fragment class.
  * This added functionality is useful for fragments in this app.
  */
-public class Fragment extends android.support.v4.app.Fragment {
+public class Fragment extends androidx.fragment.app.Fragment {
 
 	/** Returns the activity for this app cast as a MainActivity. */
 	@NotNull
--- Fragments/app/src/main/java/edu/byui/cit/fragments/SecondFrag.java
+++ Fragments/app/src/main/java/edu/byui/cit/fragments/SecondFrag.java
@@ -2,7 +2,7 @@ package edu.byui.cit.fragments;
 
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
