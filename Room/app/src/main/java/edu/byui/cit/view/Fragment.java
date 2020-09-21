d660a9261c19dcd250335ca154428e4040334184
Rex Barzee <barzeer@byui.edu>
2020-09-21 17:29:30 -0600

    Update all example apps to the latest gradle.

---
+++ Room/app/src/main/java/edu/byui/cit/view/Fragment.java
@@ -0,0 +1,15 @@
+package edu.byui.cit.view;
+
+import androidx.annotation.NonNull;
+
+
+/**
+ * This class simply adds functionality to the Android Fragment class.
+ * This added functionality is useful for fragments in this app.
+ */
+public class Fragment extends androidx.fragment.app.Fragment {
+	@NonNull
+	public MainActivity getMainActivity() {
+		return (MainActivity)getActivity();
+	}
+}
---
+++ Room/app/src/main/java/edu/byui/cit/view/MainFrag.java
@@ -0,0 +1,51 @@
+package edu.byui.cit.view;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+
+
+public final class MainFrag extends Fragment {
+	@Override
+	public View onCreateView(@NonNull LayoutInflater inflater,
+			ViewGroup container, Bundle savedInstanceState) {
+		super.onCreateView(inflater, container, savedInstanceState);
+		View view = inflater.inflate(R.layout.frag_main, container, false);
+
+		// Display the username in the username text field.
+		MainActivity act = getMainActivity();
+		EditText txtUsername = view.findViewById(R.id.txtUsername);
+		txtUsername.setText(act.getUsername());
+		txtUsername.setOnFocusChangeListener(new HandleUsername());
+
+		// Add an action listener to each button.
+		Button button = view.findViewById(R.id.btnRoom);
+		button.setOnClickListener(act.roomClickHandler);
+
+		return view;
+	}
+
+
+	private final class HandleUsername implements View.OnFocusChangeListener {
+		@Override
+		public void onFocusChange(View view, boolean hasFocus) {
+			try {
+				if (!hasFocus) {
+					MainActivity act = getMainActivity();
+					EditText txtUsername = (EditText)view;
+					String username = txtUsername.getText().toString().trim();
+					act.setUsername(username);
+				}
+			}
+			catch (Exception ex) {
+				Log.e(MainActivity.TAG, ex.getMessage());
+			}
+		}
+	}
+}
